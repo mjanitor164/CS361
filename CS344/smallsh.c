63cdd0eb5e3aaac67a758bb7bddd8af39c9ae132
Megan Janitor <janitorm@oregonstate.edu>
2021-11-02 13:21:15 -0400

    fixed curly brace formatting 1:21

--- CS344/smallsh.c
+++ CS344/smallsh.c
@@ -49,8 +49,7 @@ pid_t initPid = -1;
 int childExitMethod = -1;
 
 
-void main()
-{
+void main(){
 	// initialize struct for SIGINT later
 	struct sigaction SIGINT_action = { 0 };
 	// initialize struct fot SIGTSTP later
@@ -66,8 +65,7 @@ void main()
 	sigfillset(&SIGTSTP_action.sa_mask);
 	sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
-	while (keepRunning == 1)
-	{
+	while (keepRunning == 1){
 		// checks what background processes are still running
 		isProcessRunning();					
 		getInput();		
@@ -80,8 +78,7 @@ void main()
 // Catches SIGINT (CTRL+C). Parent process, child background processes all ignore SIGINT,
 // while a child running as a foreground process will terminate itself upon receiving
 // SIGINT. Number of the signal that killed this child process will be displayed after.
-void catchSIGINT(int signo)
-{
+void catchSIGINT(int signo){
 	printf("terminated by signal %d\n", signo);
 	fflush(stdout);
 }
@@ -90,11 +87,9 @@ void catchSIGINT(int signo)
 // while the parent process running the shell will toggle foreground-only mode on and off.
 // If on, turns off, if off, turns on. Foreground-only mode forces all new processes to be run in the
 // foreground, even ones specified to be background processes with &.
-void catchSIGTSTP()
-{
+void catchSIGTSTP(){
 	// if flag is false, that means that foreground-only mode is off and we turn it on (it is toggled)
-	if (sigtstpFlag == false)
-	{
+	if (sigtstpFlag == false){
 		// resets sigNum in case of previous SIGTSTP
 		sigNum = 0;
 		backgroundFlag = false;
@@ -108,8 +103,7 @@ void catchSIGTSTP()
 		sigNum++;								
 	}
 	// otherwise, foreground-only mode is on and we turn it off
-	else
-	{
+	else{
 		// resets the flag for later
 		sigtstpFlag = false;					
 
@@ -125,25 +119,20 @@ void catchSIGTSTP()
 // checks on background processes to see which are still running, since
 // parent runs asynchronously. Displays a termination message including
 // pid if a process was terminated.
-void isProcessRunning()
-{
+void isProcessRunning(){
 	int i;
 
-	for (i = 0; i < numProcesses; i++)	
-	{
+	for (i = 0; i < numProcesses; i++)	{
 		// if waiting
-		if (waitpid(processes[i], &childExitMethod, WNOHANG) > 0)
-		{
+		if (waitpid(processes[i], &childExitMethod, WNOHANG) > 0){
 			// if child process was terminated
-			if (WIFSIGNALED(childExitMethod))
-			{
+			if (WIFSIGNALED(childExitMethod)){
 				// displays pid of child as well as terminating signal number
 				printf("background pid terminated is %d\n", processes[i]);
 				printf("terminated by signal %d\n", WTERMSIG(childExitMethod));
 			}
 			// check if process was terminated normally
-			if (WIFEXITED(childExitMethod))
-			{
+			if (WIFEXITED(childExitMethod)){
 				// prints in this format as per sample program execution
 				printf("exit value %d\n", WEXITSTATUS(childExitMethod));
 			}
@@ -154,8 +143,7 @@ void isProcessRunning()
 
 // Reads each line from the user, checks for & (run in background command),
 // and $$ (can be expanded). Formats smallsh command line with ":" on each line.
-void getInput()
-{
+void getInput(){
 	// holds 4 chars of input (need 5 to include 0/)
 	char lineInput[5] = { 0 };
 
@@ -170,27 +158,27 @@ void getInput()
 	strncpy(lineInput, input, 4);
 
 	// if not output and & is present, run in background
-	if (strcmp(lineInput, "echo") != 0)	
-	{
-		if (strchr(input, '&') != NULL)	
-		{
+	if (strcmp(lineInput, "echo") != 0)	{
+		if (strchr(input, '&') != NULL)	{
 			runInBackground();
 		}
 	}
 	// see if command is SIGTSTP or needs to be expanded ($$ indicator)
-	if (strstr(input, "TSTP") == NULL)
-		if (strstr(input, "$$") != NULL)
+	if (strstr(input, "TSTP") == NULL){
+		if(strstr(input, "$$") != NULL){
 			expand();
+		}
+	}
 	
 	// see if command is SIGTSTP and catch it if it is
-	if (strstr(input, "TSTP") != NULL)
-		catchSIGTSTP();	
+	if (strstr(input, "TSTP") != NULL){
+		catchSIGTSTP();
+	}
 }
 
 
 // Removes newline character for nicer look
-void removeEndChar(char *removeItem)
-{
+void removeEndChar(char *removeItem){
 	// change to null char
 	removeItem[strcspn(removeItem, "\n")] = '\0';
 }
@@ -198,15 +186,15 @@ void removeEndChar(char *removeItem)
 
 // Checks to see if process is able to be run in the background,
 // sets a flag if so
-void runInBackground()
-{
+void runInBackground(){
 	char backgroundCommand[MAX_CMD_LENGTH] = { 0 };
 	// input string needs to be modified to remove the & symbol (because background process)
 	// as well as the space
 	int size = (strlen(input) - 2);					
 	// Runs in background if there's no SIGTSTP
-	if(sigtstpFlag == false)
+	if(sigtstpFlag == false){
 		backgroundFlag = true;
+	}
 	// Adds this process into the backgroundCommand array minus last two characters
 	strncpy(backgroundCommand, input, size);
 	strcpy(input, backgroundCommand);
@@ -214,8 +202,7 @@ void runInBackground()
 
 // Expands any instance of $$ in a command into the process ID of of the smallsh itself
 // This copies the string and modifies it based on the length
-void expand()
-{
+void expand(){
 	char inputArray[MAX_CMD_LENGTH] = { 0 };
 	// sets size to be length minus 2--removing "$$"
 	int size = (strlen(input) - 2);
@@ -231,8 +218,7 @@ void expand()
 
 // Expands $$ into a process like expand() does, but this function is called
 // after the fork instead and uses the child's pid.
-void expandFromChild()
-{
+void expandFromChild(){
 	// input array
 	char expandFromChild[MAX_CMD_LENGTH] = { 0 };
 	//  modify length--must be size minus 11--this removes the signal and "$$"
@@ -250,11 +236,9 @@ void expandFromChild()
 
 // Processes the build in shell commands (cd, status, exit), as well
 // as comments (denoted by #)
-void doBuiltIns()
-{
+void doBuiltIns(){
 	// handles cd built-in
-	if (strncmp(input, "cd", 2) == 0)			
-	{
+	if (strncmp(input, "cd", 2) == 0){
 		// array for directories
 		char cwd[MAX_CMD_LENGTH];
 		char *newPath;
@@ -262,17 +246,15 @@ void doBuiltIns()
 		getcwd(cwd, sizeof(cwd));
 		// adds cd to path
 		newPath = strstr(input, " ");
-		if (newPath)
-		// this handles the formatting needed to change directory into the new directory
-		{
+		if (newPath){
+			// this handles the formatting needed to change directory into the new directory
 			newPath++;
 			strcat(cwd, "/");
 			strcat(cwd, newPath);
 			chdir(cwd);	
 		}
 		// if not a new directory, just use home directory
-		else
-		{
+		else{
 			chdir(getenv("HOME"));
 		}
 		// get new current directory and output it to terminal (as required for the assignment)
@@ -281,26 +263,22 @@ void doBuiltIns()
 		fflush(stdout);
 	}
 	// handles built-in status command
-	else if (strcmp(input, "status") == 0)
-	{
+	else if (strcmp(input, "status") == 0){
 		// print the exit value to terminal as required for the assignment
 		printf("exit value %d\n", status);
 		fflush(stdout);
 	}
 	// handles the built-in exit command
-	else if (strcmp(input, "exit") == 0)
-	{
+	else if (strcmp(input, "exit") == 0){
 		// sets this variable to 0 which will end the program
 		keepRunning = 0;
 	}
 	// causes shell to ignore comments (ones with # or blank spaces)
-	else if (strncmp(input, "#", 1) == 0 || strcmp(input, " ") == 0)
-	{
+	else if (strncmp(input, "#", 1) == 0 || strcmp(input, " ") == 0){
 		//Do nothing
 	}
 	// if nothing else, we fork
-	else
-	{
+	else{
 		forkProcess();
 	}
 	// reset this flag for later in case it was flagged earlier
@@ -311,25 +289,20 @@ void doBuiltIns()
 // Contains logic to cause the parent to wait or continue
 // when the child is made, depending on if the child is a 
 // background or foreground process
-void forkProcess()
-{
+void forkProcess(){
 	initPid = fork();
 
 	// check to make sure it forked correctly (error returns negative number)
-	if (initPid < 0)
-	{
+	if (initPid < 0){
 		printf("Error Forking\n");
 		fflush(stdout);
 		exit(1);
 	}
-	else if (initPid == 0)
-	{
+	else if (initPid == 0){
 		// check for sigtstp flag
-		if (sigNum > 0)	
-		{
+		if (sigNum > 0)	{
 			// make sure there isn't a bounty out for the child process
-			if (strstr(input, "kill") != NULL)
-			{
+			if (strstr(input, "kill") != NULL){
 				expandFromChild();
 			}
 
@@ -337,13 +310,11 @@ void forkProcess()
 		// run commands for the child
 		executeCommand();
 	}
-	else
-	{
+	else{
 		// if process is running in background, we add the child to the processes array,
 		// increase process counter, ensure parent won't wait for child, reset background
 		// flag, and output message to terminal with child's pid
-		if (backgroundFlag == true)
-		{
+		if (backgroundFlag == true){
 			processes[numProcesses] = initPid;
 			numProcesses++;	
 			waitpid(initPid, &childExitMethod, WNOHANG);
@@ -351,13 +322,13 @@ void forkProcess()
 			printf("background pid is %d\n", initPid);
 			fflush(stdout);
 		}
-		else
-		// if not in background, cause the parent to wait for the child process to finish first,
-		// check for errors, and set error flag if so
-		{
+		else{
+			// if not in background, cause the parent to wait for the child process to finish first,
+			// check for errors, and set error flag if so
 			waitpid(initPid, &childExitMethod, 0);
-			if (WIFEXITED(childExitMethod))
+			if (WIFEXITED(childExitMethod)){
 				status = WEXITSTATUS(childExitMethod);
+			}
 		}
 	}
 }
@@ -366,8 +337,7 @@ void forkProcess()
 // Once a child process is made, executeCommand runs the command.
 // This reads the full command, opens or creates files if directed,
 // sets pointers, and handles errors.
-void executeCommand()
-{
+void executeCommand(){
 	char* commandArgv[512];
 	int count = 0;
 	int i = 0;
@@ -378,37 +348,31 @@ void executeCommand()
 	// gets the command
 	commandArgv[0] = strtok(input, " ");
 	// processes all of the commands
-	while (commandArgv[count] != NULL)
-	{
+	while (commandArgv[count] != NULL){
 		count++;
 		// add the command to the array									
 		commandArgv[count] = strtok(NULL, " ");
 	}
 	// keep going while there is still data to parse
-	while (count != 0)
-	{
+	while (count != 0){
 		// need to check for read-only instruction
-		if (strcmp(commandArgv[i], "<") == 0)
-		{
+		if (strcmp(commandArgv[i], "<") == 0){
 			// if so open file as readonly and check for errors before exiting
 			fileDesc = open(commandArgv[i + 1], O_RDONLY, 0);
-			if (fileDesc < 0)
-			{
+			if (fileDesc < 0){
 				// error message
 				printf("cannot open %s for input\n", commandArgv[i + 1]);
 				fflush(stdout);
 				exit(1);
 			}
-			else
-			// if no errors, set pointer to stdin and set redirect flag
-			{
+			else{
+				// if no errors, set pointer to stdin and set redirect flag
 				std = 0;
 				redirect = 1;
 			}
 		}
 		// check for write-only instruction
-		else if (strcmp(commandArgv[i], ">") == 0)
-		{
+		else if (strcmp(commandArgv[i], ">") == 0){
 			// if so open file as writeonly and set pointer to stdout
 			fileDesc = open(commandArgv[i + 1], O_CREAT | O_WRONLY, 0755);
 			std = 1;
@@ -416,8 +380,7 @@ void executeCommand()
 		}
 
 		// if we need to do output redirection
-		if (redirect == 1)
-		{
+		if (redirect == 1){
 			// handles output redirection as instructed in assignment specs
 			dup2(fileDesc, std);
 			// clear it from the array
@@ -437,8 +400,7 @@ void executeCommand()
 		std = -2;
 	}
 	// if command was not a redirect, execute it normally
-	if(status=execvp(commandArgv[0], commandArgv) != 0);
-	{
+	if(status=execvp(commandArgv[0], commandArgv) != 0);{
 		// displays error if error occurred
 		printf("%s: no such file or directory\n", input);
 		exit(status);
